3b5aadf83d2087591bb2f4eff68023504cb89b8a
Sarine Hur <veryshur@gmail.com>
2014-07-27 07:41:24 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackersAndHounds::Application.config.secret_key_base = 'f26e932cbc400c27a9b209bb35be9828a75046ecb708419b68d939cb805eb58be472d14ca65addea5ec02b38638bdae16d6b9280b98d0b69c148fcb17b5b3d60'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HackersAndHounds::Application.config.session_store :cookie_store, key: '_hackers_and_hounds_session'
