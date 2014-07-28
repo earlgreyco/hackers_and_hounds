11e5ab54ec1ab4f653e6bda6ba820b5fdc59f5ea
Sarine Hur <veryshur@gmail.com>
2014-07-27 18:45:30 -0700

    added user model and new article form

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716143830) do
+ActiveRecord::Schema.define(version: 20140727212630) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
